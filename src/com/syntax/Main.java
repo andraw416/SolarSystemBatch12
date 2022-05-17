6a36fe17929c60e653f3aa6304d169c99ff5ae31
andraw416 <andrewrawlik@gmail.com>
2022-05-17 19:16:53 -0400

    Solar System Basic Project ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,13 @@
+package com.syntax;
+
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem xfac=new SolarSystem();
+
+        xfac.moon="1";
+        xfac.planet="4";
+        xfac.sun="6";
+        xfac.star="1000";
+        
+    }
+}
